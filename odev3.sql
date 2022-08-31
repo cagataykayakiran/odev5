e158f497f7d0e5b7c1c65791a94f9ed898522376
Çağatay Kayakıran <73523962+cagataykayakiran@users.noreply.github.com>
2022-08-31 15:42:32 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,11 @@
+select * from country
+where country like 'A%a';
+
+select * from country
+where country like '______%n';
+
+select title from film
+where title ilike '%T%T%T%T%'
+
+select * from film
+where title like 'C%' and length > 90 and rental_rate = 2.99;
