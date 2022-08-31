d871ba407a8a0acc150eac683c0412d986d805d1
Çağatay Kayakıran <73523962+cagataykayakiran@users.noreply.github.com>
2022-08-31 15:42:52 +0300

    Create odev4.sql

---
+++ odev4.sql
@@ -0,0 +1,12 @@
+SELECT  count(distinct replacement_cost) from film;
+
+select distinct replacement_cost from film;
+
+select count (*) title from film
+where title like 'T%' and rating = 'G';
+
+select count (*) country from country
+where country like '_____';
+
+select count(*) from city
+where city ilike '%R';
